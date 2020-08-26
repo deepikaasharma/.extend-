3c6bd98bf4fcdfff6d6f5af9ff6473f9b336e7e3
Deepika Sharma <deepikaasharma@outlook.com>
2020-08-26 23:30:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# num_list = [1, 3, 5, 7, 9]
+# other_list = [7, 3, 55]
+
+# # Extend num_list with the contents of other_list
+# num_list.extend(other_list)
+
+# # Prints [1, 3, 5, 7, 9, 7, 3, 55]
+# print(num_list)
+
+str_list = ['a', 'e', 'i', 'o', 'u']
+other_str = 'AEIOU'
+
+# Extend str_list with the capital vowels
+str_list.extend(other_str)
+
+# Prints ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+print(str_list)
